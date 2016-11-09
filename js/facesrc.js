7d434a5a6d23afab53aa25d8727ddcb0b0b123ec
zhangchi <903430068@qq.com>
2016-11-09 18:39:23 +0800

    js

---
+++ js/facesrc.js
@@ -0,0 +1,18 @@
+// facesrc.js
+
+function preview(file)  
+{  
+    var prevDiv = document.getElementById('preview');  
+    if (file.files && file.files[0])  
+    {  
+        var reader = new FileReader();  
+        reader.onload = function(evt){  
+        prevDiv.innerHTML = '<img class="image" src="' + evt.target.result + '" />';  
+        }    
+        reader.readAsDataURL(file.files[0]);  
+    }  
+    else    
+    {  
+        prevDiv.innerHTML = '<div class="div_img" style="filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale,src=\'' + file.value + '\'"></div>';  
+    }  
+} 
